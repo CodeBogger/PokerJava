9e0922666555c8cee950bc69e73452ac6ae61d5e
Boggers <90660532+CodeBogger@users.noreply.github.com>
2023-08-31 21:28:05 -0400

    Update PlayerBot.java stuff

--- src/main/java/com/example/javapoker/PlayerBot.java
+++ src/main/java/com/example/javapoker/PlayerBot.java
@@ -1,42 +1,46 @@
 package com.example.javapoker;
 
-import java.util.Scanner;
 import java.util.Random;
 
 public class PlayerBot extends Player {
-    static Scanner scan = new Scanner(System.in);
+    static Random rand = new Random();
     public PlayerBot(int chipCount, String playerName) {
         super(chipCount, playerName);
     }
     public static TurnLogic.CHOICE botTurn() {
-        Random rand = new Random();
         TurnLogic.CHOICE[] choices = TurnLogic.CHOICE.values();
+        return choices[rand.nextInt(choices.length)];
+    }
+    public void allIn() { this.chips = 0; }
+    public static TurnLogic.CHOICE allInFold(int allInAmount, Player bot) {
+        if(bot.chips >= allInAmount) {
+            TurnLogic.CHOICE[] choices = { TurnLogic.CHOICE.CALL, TurnLogic.CHOICE.ALLIN, TurnLogic.CHOICE.FOLD };
+            return choices[rand.nextInt(choices.length)];
+        }
+        TurnLogic.CHOICE[] choices = {TurnLogic.CHOICE.ALLIN, TurnLogic.CHOICE.FOLD};
         return choices[rand.nextInt(choices.length)];
     }
     public static TurnLogic.CHOICE preFlopTurn(Player player) {
-        TurnLogic.CHOICE choice = null;
-        Random rand = new Random();
         TurnLogic.CHOICE[] choices;
 
-        if(player.getBlindType() == BlindType.BIGBLIND) {
-            choices = new TurnLogic.CHOICE[] { TurnLogic.CHOICE.CHECK, TurnLogic.CHOICE.RAISE, TurnLogic.CHOICE.FOLD };
+        if (player.getBlindType() == BlindType.BIGBLIND) {
+            choices = new TurnLogic.CHOICE[]{TurnLogic.CHOICE.CHECK, TurnLogic.CHOICE.RAISE, TurnLogic.CHOICE.FOLD, TurnLogic.CHOICE.ALLIN};
             return choices[rand.nextInt(choices.length)];
         }
 
-        choices = new TurnLogic.CHOICE[] { TurnLogic.CHOICE.CALL, TurnLogic.CHOICE.RAISE, TurnLogic.CHOICE.FOLD };
+        choices = new TurnLogic.CHOICE[]{TurnLogic.CHOICE.CALL, TurnLogic.CHOICE.RAISE, TurnLogic.CHOICE.FOLD, TurnLogic.CHOICE.ALLIN};
         return choices[rand.nextInt(choices.length)];
     }
-    public static TurnLogic.CHOICE callFoldRaise(int raise) {
-        Random rand = new Random();
-        TurnLogic.CHOICE[] choices = TurnLogic.CHOICE.values();
-
-        TurnLogic.CHOICE randomChoice = choices[rand.nextInt(choices.length)];
-
-        System.out.println();
-        return TurnLogic.CHOICE.CALL;
+    public static TurnLogic.CHOICE callFoldRaise(int raise, Player player) {
+        if(player.chips - raise < 0) {
+            TurnLogic.CHOICE[] choices = { TurnLogic.CHOICE.ALLIN, TurnLogic.CHOICE.FOLD };
+            return choices[rand.nextInt(choices.length)];
+        }
+        TurnLogic.CHOICE[] choices = { TurnLogic.CHOICE.CALL, TurnLogic.CHOICE.RAISE, TurnLogic.CHOICE.FOLD, TurnLogic.CHOICE.ALLIN };
+        return choices[rand.nextInt(choices.length)];
     }
     public static int raiseTo() {
-        return 40;
+        return 80;
     }
 
 }
